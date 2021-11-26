a8208f69e242c78d3831812e01b9a99204b973ee
Bwiggemans <b.wiggemans@home.nl>
2021-11-26 09:33:49 +0100

    spaties gefatsoeneerd

--- src/main/java/nl/novi/hello/model/Book.java
+++ src/main/java/nl/novi/hello/model/Book.java
@@ -36,37 +36,28 @@ public class Book {
     }
 
     // setters and getters
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public String getIsbn() {
         return isbn;
     }
-
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
-
 }
